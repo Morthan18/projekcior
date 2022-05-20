a6f8c2f2bb040e2ae58e82de815c25a8a0e119b6
Krzysiek Snopko <krzysztof.snopko@wp.pl>
2022-05-20 14:30:45 +0200

    Users list for admin

--- src/main/java/com/projekcior/AdminController.java
+++ src/main/java/com/projekcior/AdminController.java
@@ -1,14 +1,53 @@
 package com.projekcior;
 
-import org.springframework.security.access.annotation.Secured;
+import com.projekcior.model.UserRepository;
+import lombok.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class AdminController {
 
+    private final UserRepository userRepository;
+
     @GetMapping("/admin")
-    public String admin() {
-        return "admin";
+    public ModelAndView admin() {
+        ModelAndView mav = new ModelAndView("admin");
+        var users = userRepository.findAll()
+                .stream()
+                .filter(u -> u.getFirstName() != null)
+                .map(u -> {
+                    var us = new UserToEdit();
+                    us.setId(u.getId());
+                    us.setFirstName(u.getFirstName());
+                    us.setLastName(u.getLastName());
+                    us.setAge(u.getAge());
+                    return us;
+                })
+                .toList();
+        mav.addObject("users", users);
+        return mav;
+    }
+
+
+
+    @Data
+    @NoArgsConstructor
+    static class UserToEdit {
+        private long id;
+
+        private String firstName;
+
+        private String lastName;
+
+        private Integer age;
+
     }
 }
--- src/main/java/com/projekcior/SecurityConfig.java
+++ src/main/java/com/projekcior/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.projekcior;
 
+import com.projekcior.model.Authority;
+import com.projekcior.model.User;
+import com.projekcior.model.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -18,20 +25,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.util.List;
 
 @CrossOrigin("*")
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements ApplicationListener {
 
     private final DataSource dataSource;
+    private final UserRepository userRepository;
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-                .authorizeRequests().antMatchers("/admin").hasRole("ADMIN")
+                .authorizeRequests().antMatchers("/admin**").hasRole("ADMIN")
                 .and()
                 .authorizeRequests().antMatchers(
                         "/",
@@ -43,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin()
                 .loginPage("/login.html")
-                .failureHandler(new SimpleUrlAuthenticationFailureHandler(){
+                .failureHandler(new SimpleUrlAuthenticationFailureHandler() {
                     @Override
                     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
 
@@ -69,5 +78,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ContextRefreshedEvent) {
+            userRepository.save(new User("Krzycho", "Snobko", "krzycho_jp_300_PL", passwordEncoder().encode("123"), 24, List.of(new Authority("krzycho_jp_300_PL", "USER"))));
+            userRepository.save(new User("Krzycho", "ADMIN", "ADMINOS", passwordEncoder().encode("123"), 24, List.of(new Authority("ADMINOS", "ADMIN"))));
+        }
     }
 }
